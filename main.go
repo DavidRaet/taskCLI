01a0c96d60476077cc110efc63293eae270ea7f5
DavidRaet <draet@vassar.edu>
2026-04-21 21:19:08 -0400

    feat: Define Task struct and createTask function that takes in an ID and a title. The function was used in the main and prints out the ID, title, status, and it's creation date.

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
+type Status struct {
+	todo       string
+	inProgress string
+	done       bool
+}
 
-func main(){
-	fmt.Println("Task CLI running")
+type Task struct {
+	ID        int
+	Title     string
+	Status    Status
+	CreatedAt string
+	UpdatedAt string
+}
+
+func createTask(id int, title string) Task {
+	return Task{
+		ID:        id,
+		Title:     title,
+		Status:    Status{todo: "pending"},
+		CreatedAt: time.Now().String(),
+		UpdatedAt: time.Now().String(),
+	}
+}
+
+func main() {
+	newTask := createTask(123, "New Task")
+
+	fmt.Printf("Hello new task: %s\n", newTask.Title)
+	fmt.Printf("New task ID: %d\n", newTask.ID)
+	fmt.Printf("Task status: %s\n", newTask.Status.todo)
+	fmt.Printf("Task created at: %s\n", newTask.CreatedAt)
 }
