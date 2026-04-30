8d876dd12958415a30b75f41d692a5b90838b151
DavidRaet <draet@vassar.edu>
2026-04-29 20:35:44 -0400

    feat: import os and encoding/json Go libraries and add json tags for the Task struct and Status struct.

--- main.go
+++ main.go
@@ -3,27 +3,29 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"encoding/json"
 )
 
 type Status struct {
-	todo       string
-	inProgress string
-	done       bool
+    Todo       string `json:"todo"`
+    InProgress string `json:"inProgress"`
+    Done       bool   `json:"done"`
 }
 
 type Task struct {
-	ID        int
-	Title     string
-	Status    Status
-	CreatedAt string
-	UpdatedAt string
+    ID        int    `json:"id"`
+    Title     string `json:"title"`
+    Status    Status `json:"status"`
+    CreatedAt string `json:"createdAt"`
+    UpdatedAt string `json:"updatedAt"`
 }
 
 func createTask(id int, title string) Task {
 	return Task{
 		ID:        id,
 		Title:     title,
-		Status:    Status{todo: "pending"},
+		Status:    Status{Todo: "pending"},
 		CreatedAt: time.Now().String(),
 		UpdatedAt: time.Now().String(),
 	}
